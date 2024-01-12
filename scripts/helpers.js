a158bf1f0a583f93e70362b36172244da6bc9269
tanmayc05 <113726318+tanmayc05@users.noreply.github.com>
2024-01-12 02:55:56 -0600

    move function to helpers file to clean up main

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,23 +1,47 @@
-// make fish zoom out when added to world
+import { Fish } from "./fish.js";
 
-export function zoomOut(fish, engine) {
-    const body = fish.getBody();
-    const position = body.position;
-    const scale = body.render.sprite.xScale;
-    const newScale = scale - 0.01;
-    const newRadius = fish.getRadius() * newScale;
-    const newBody = Matter.Bodies.circle(position.x, position.y, newRadius, {
-        render: {
-            sprite: {
-                texture: fish.getSprite(),
-                xScale: newScale,
-                yScale: newScale,
-            },
-        },
+
+export const fishSettledTimes = new Map(); // Map to store timeouts for each fish
+
+export function isCanvasFilled(engine, loseBoundary, lineBoundary, drawLineSettled) {
+    engine.world.bodies.forEach((body) => {
+        if (body.owner instanceof Fish && !body.isStatic) {
+            const fish = body.owner;
+            const bottomOfFish = body.position.y + body.circleRadius;
+
+            // Check if the fish has settled above the line
+            if (
+                bottomOfFish < loseBoundary &&
+                Matter.Vector.magnitude(body.velocity) < 0.1 &&
+                Math.abs(body.angularVelocity) < 0.1
+            ) {
+
+                if (!fishSettledTimes.has(fish)) {
+                    // If the fish is settled above the line for the first time, record the timestamp
+                    fishSettledTimes.set(fish, Date.now());
+                }
+            } 
+            if (
+                bottomOfFish < lineBoundary &&
+
+                Matter.Vector.magnitude(body.velocity) < 0.1 &&
+                Math.abs(body.angularVelocity) < 0.1
+            ) {
+                drawLineSettled = true;
+            }
+            else {
+                // If the fish is not above the line, remove it from the settled times map
+                fishSettledTimes.delete(fish);
+            }
+        }
     });
-    newBody.owner = fish;
-    Matter.World.add(engine.world, newBody);
-    Matter.World.remove(engine.world, body);
-    fish.setBody(newBody);
-    
-} 
+    const currentTime = Date.now();
+    const settledFish = [...fishSettledTimes.entries()].find(
+        ([, timestamp]) => currentTime - timestamp >= 1000
+    );
+
+    return {
+        isGameOver: settledFish !== undefined,
+        shouldDrawLine: drawLineSettled,
+    };
+}
--- scripts/physics.js
+++ scripts/physics.js
@@ -3,7 +3,7 @@
 var Engine = Matter.Engine
 
 
-export function createUnderwaterEngine() {
+export function createEngine() {
     const engine = Engine.create();
     engine.world.gravity.y = 0.25;
     return engine;
